ee66633ba1e78ddf0892e969b9f3e7cdc3a9457a
Juan Lopez <lopez5@hotmail.com>
2026-02-02 15:36:56 -0500

    Adding new control

---
+++ src/CoreOne.Winforms/Controls/CSplitter.cs
@@ -0,0 +1,61 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace CoreOne.Winforms.Controls;
+
+public class CSplitter : SplitContainer
+{
+    private Color PHandleColor;
+
+    public Color HandleColor {
+        get => PHandleColor;
+        set {
+            PHandleColor = value;
+            Transparent = Color.FromArgb(0, value);
+            Invalidate();
+        }
+    }
+
+    public Color Transparent { get; private set; }
+
+    public CSplitter()
+    {
+        Paint += CSplitter_Paint;
+    }
+
+    public float GetAngle() => Orientation == Orientation.Horizontal ? 180 : 90;
+
+    public Rectangle GetSplitterBounds() => Orientation == Orientation.Horizontal ?
+            new Rectangle(0, SplitterDistance, Width, SplitterWidth) :
+            new Rectangle(SplitterDistance, 0, SplitterWidth, Height);
+
+    private void CSplitter_Paint(object? sender, PaintEventArgs e)
+    {
+        LinearGradientBrush brush;
+        Rectangle
+            splitterarea,
+            drawingarea;
+
+        //Color highlight;
+        var blend = new ColorBlend {
+            Colors = [Transparent, HandleColor, Transparent],
+            Positions = [0, 0.5F, 1]
+        };
+        float angle = GetAngle();
+        splitterarea = GetSplitterBounds();
+        if (Orientation == Orientation.Horizontal)
+        {
+            drawingarea = splitterarea;
+            drawingarea.Inflate(Convert.ToInt32(Width * -0.1F), -1);
+        }
+        else
+        {
+            drawingarea = splitterarea;
+            drawingarea.Inflate(-1, Convert.ToInt32(Height * -0.1F));
+        }
+        using (brush = new LinearGradientBrush(drawingarea, HandleColor, Transparent, angle))
+        {
+            brush.InterpolationColors = blend;
+            e.Graphics.FillRectangle(brush, drawingarea);
+        }
+    }
+}
